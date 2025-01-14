4106d65c59d4ed85806f979d4581450c41b54a05
Jordan <jordan_account2@outlook.com>
2025-01-14 12:25:15 -0500

    Set up the Menu and Booking API

--- workspace/littlelemon/restaurant/urls.py
+++ workspace/littlelemon/restaurant/urls.py
@@ -1,6 +1,13 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 from . import views
+
+router = DefaultRouter()
+router.register(r'tables', views.BookingViewSet) # Maps '/tables/' route to BookingViewSet for CRUD operations
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('menu/', views.MenuItemsView.as_view()),
+    path('menu/<int:pk>', views.SingleMenuItemView.as_view()),
+    path('booking/', include(router.urls)),
 ]
